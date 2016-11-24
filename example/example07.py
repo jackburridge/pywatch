6769d80b521bedba5818f1d51a8b42a1612d9633
jburridge <jack.burridge@mail.com>
2016-11-24 18:51:24 +0000

    Added some buttons to example07 to be able to add or remove elements from the list

--- example/example07.py
+++ example/example07.py
@@ -19,12 +19,19 @@ class MyFrame(wx.Frame):
 
         list_boxChoices = []
         self.list_box = wx.ListBox(self, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize, list_boxChoices, 0)
-        gb_sizer.Add(self.list_box, wx.GBPosition(0, 0), wx.GBSpan(1, 1), wx.EXPAND, 0)
+        gb_sizer.Add(self.list_box, wx.GBPosition(0, 0), wx.GBSpan(1, 2), wx.EXPAND, 0)
+
+        self.add_button = wx.Button(self, wx.ID_ANY, u"Add", wx.DefaultPosition, wx.DefaultSize, 0)
+        gb_sizer.Add(self.add_button, wx.GBPosition(1, 0), wx.GBSpan(1, 1), wx.EXPAND, 0)
+
+        self.remove_button = wx.Button(self, wx.ID_ANY, u"Remove", wx.DefaultPosition, wx.DefaultSize, 0)
+        gb_sizer.Add(self.remove_button, wx.GBPosition(1, 1), wx.GBSpan(1, 1), wx.EXPAND, 0)
 
         self.text_ctrl = wx.TextCtrl(self, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.DefaultSize, 0)
-        gb_sizer.Add(self.text_ctrl, wx.GBPosition(1, 0), wx.GBSpan(1, 1), wx.EXPAND, 0)
+        gb_sizer.Add(self.text_ctrl, wx.GBPosition(2, 0), wx.GBSpan(1, 2), wx.EXPAND, 0)
 
         gb_sizer.AddGrowableCol(0)
+        gb_sizer.AddGrowableCol(1)
         gb_sizer.AddGrowableRow(0)
 
         sizer.Add(gb_sizer, 1, wx.EXPAND | wx.ALL, 5)
@@ -33,6 +40,9 @@ class MyFrame(wx.Frame):
         self.Layout()
 
         self.Centre(wx.BOTH)
+
+        self.add_button.Bind(wx.EVT_BUTTON, self.on_add)
+        self.remove_button.Bind(wx.EVT_BUTTON, self.on_remove)
 
         self.model = WatchableDict()
         self.model["list"] = ["one", "two", "three", "four"]
@@ -48,6 +58,14 @@ class MyFrame(wx.Frame):
             self.model["list"][self.model["selection"]] = value
 
         pywatch.wx.ValueChanger(self.text_ctrl, self.model, ("list", "selection", setter, getter))
+
+    def on_add(self, event):
+        self.model["list"].append(self.model["list"][self.model["selection"]])
+        event.Skip()
+
+    def on_remove(self, event):
+        self.model["list"].pop(self.model["selection"])
+        event.Skip()
 
 
 if __name__ == "__main__":
