cd702e19b806bf2f4819ef25cbc0dbce9f724f79
jburridge <jack.burridge@mail.com>
2016-11-25 11:59:43 +0000

    added setup file

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='pywatch',
+    version='0.1',
+    packages=['pywatch', 'pywatch.wx'],
+    url='',
+    license='',
+    author='jack',
+    author_email='',
+    description='A python MVVC solution'
+)
